f17b97068e083aefded7d024b4e2a558d37f5eb4
Aqief Syahmi <aqiefsyahmi@gmail.com>
2023-08-07 00:29:17 +0800

    make popup message in image repo

--- app/Views/old-file/form/image_repo.php
+++ app/Views/old-file/form/image_repo.php
@@ -17,12 +17,20 @@
                     <div class="ui one column centered grid">
                         <div class="column">
                             <div class="bold-text" style="margin-top: 10px;">Upload Image:</div>
+<<<<<<< HEAD:app/Views/old-file/form/image_repo.php
                             <input type="file" name="memoimg" (change)="fileEvent($event)" class="inputfile" id="embedpollfileinput" aria-hidden="true">
+=======
+                            <input type="file" (change)="fileEvent($event)" class="inputfile" id="embedpollfileinput" aria-hidden="true">
+>>>>>>> d0388b1 (make popup message in image repo):app/Views/form/image_repo.php
                             <label for="embedpollfileinput" class="ui small green left floated button" id="uploadButton">
                                 <i class="ui upload icon"></i>
                                 Upload Image
                             </label>
+<<<<<<< HEAD:app/Views/old-file/form/image_repo.php
                             <input type="file" name="memoimg1" (change)="fileEvent($event)" class="inputfile1" id="embedpollfileinput1" aria-hidden="true">
+=======
+                            <input type="file" (change)="fileEvent($event)" class="inputfile1" id="embedpollfileinput1" aria-hidden="true">
+>>>>>>> d0388b1 (make popup message in image repo):app/Views/form/image_repo.php
                             <label for="embedpollfileinput1" class="ui small red left floated button" id="reuploadButton" style="display: none;">
                                 <i class="ui upload icon"></i>
                                 Reupload Image
@@ -35,7 +43,11 @@
                             <div class="bold-text">Comment:</div>
                             <div class="ui form">
                                 <div class="field">
+<<<<<<< HEAD:app/Views/old-file/form/image_repo.php
                                     <textarea name="descriptions" rows="3" placeholder="Please Enter Current Symptoms or Concerns"></textarea>
+=======
+                                    <textarea rows="3" placeholder="Please describe the image"></textarea>
+>>>>>>> d0388b1 (make popup message in image repo):app/Views/form/image_repo.php
                                 </div>
                             </div>
                             <!-- error message -->
@@ -61,23 +73,40 @@
     const fileInput1 = document.getElementById('embedpollfileinput1');
     const uploadButton = document.getElementById('uploadButton');
     const reuploadButton = document.getElementById('reuploadButton');
+    const successText = document.getElementById('successText');
 
-    // Add event listeners to the file inputs
+    // Add event listener to the file inputs
     fileInput.addEventListener('change', () => {
-        toggleButtons();
+        handleFileInputChange(fileInput.files[0]);
     });
     fileInput1.addEventListener('change', () => {
-        toggleButtons();
+        handleFileInputChange(fileInput1.files[0]);
     });
+
+    function handleFileInputChange(file) {
+        if (file && file.type === 'image/jpeg' || file && file.type === 'image/png') {
+            toggleButtons();
+            hideErrorMessage();
+        } else {
+            showErrorMessage();
+            resetFileInput(fileInput);
+            resetFileInput(fileInput1);
+        }
+    }
+
+    // Function to reset the file input value
+    function resetFileInput(input) {
+        input.value = '';
+    }
+
+    // Function to show the error message for invalid formats
+    function showErrorMessage() {
+        alert('Invalid format! Only PNG and JPG images are allowed.');
+    }
 
     // Function to toggle the visibility and disabled state of the buttons
     function toggleButtons() {
-        if (fileInput.files.length > 0) {
-            uploadButton.style.display = 'none';
-            reuploadButton.style.display = 'inline-block';
-            // Show the success text when the red button is visible and a file is selected
-            successText.style.display = 'block';
-        } else if (fileInput1.files.length > 0) {
+        if (fileInput.files.length > 0 || fileInput1.files.length > 0) {
             uploadButton.style.display = 'none';
             reuploadButton.style.display = 'inline-block';
             // Show the success text when the red button is visible and a file is selected
