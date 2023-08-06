5e8a28ecfca7604436279308af6b140f552b00d4
Haziq Musa <last.game01987@gmail.com>
2023-08-06 23:02:05 +0800

    add error message

--- app/Views/user/login_page.php
+++ app/Views/user/login_page.php
@@ -8,10 +8,30 @@
 
 <div class="ui four column centered grid" style="width: 1000vh;">
 
+
   <form action="<?= url_to('login') ?>" method="post" class="middle aligned column">
-    <?= csrf_field() ?>
+
     <h1>Login</h1>
     <div>Please insert your account detail:</div>
+
+    <!-- error message -->
+    <?php if (session('error') !== null) : ?>
+      <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
+    <?php elseif (session('errors') !== null) : ?>
+      <div class="alert alert-danger" role="alert">
+        <?php if (is_array(session('errors'))) : ?>
+          <?php foreach (session('errors') as $error) : ?>
+            <?= $error ?>
+            <br>
+          <?php endforeach ?>
+        <?php else : ?>
+          <?= session('errors') ?>
+        <?php endif ?>
+      </div>
+    <?php endif ?>
+    <?= csrf_field() ?>
+
+    <!-- email -->
     <div>
       <div class="ui labeled input fluid" style="margin-top: 10px;">
         <div class="ui black label">
@@ -20,6 +40,8 @@
         <input type="text" id="email" name="email" placeholder="Please enter email *">
       </div>
     </div>
+
+    <!-- password -->
     <div style="margin-top: 10px; margin-bottom: 10px;">
       <div class="ui labeled input fluid">
         <div class="ui black label">
@@ -28,11 +50,13 @@
         <input type="password" id="password" name="password" placeholder="Enter password *">
       </div>
     </div>
+
     <div class="ui column centered grid">
       <div style="margin-top: 10px;">
-        <button type="button" class="ui black button flat no-caps" style="margin-top: 10px; margin-left: 10px; margin-right: 10px;">Forgot Password?</button>
+        <a href="<?= url_to('magic-link') ?>" class="ui black button flat no-caps" style="margin-top: 10px; margin-left: 10px; margin-right: 10px;">Forgot Password?</a>
         <a href="<?= url_to('register') ?>" class="ui black button flat no-caps" style="margin-top: 10px; margin-left: 10px; margin-right: 10px;">Create New Account</a>
       </div>
+
       <div style="margin-top: 10px;">
         <button type="submit" class="ui black button flat no-caps">Login</button>
       </div>
--- app/Views/user/register.php
+++ app/Views/user/register.php
@@ -8,55 +8,68 @@
 
  <div class="ui two column centered grid" style="width: 150vh;">
    <div class="middle aligned column">
+
      <h1>Registration Form</h1>
      <div>Please fill in the registration details:</div>
+
+     <!-- error message -->
+     <?php if (session('error') !== null) : ?>
+       <div class="alert alert-danger" role="alert"><?= session('error') ?></div>
+     <?php elseif (session('errors') !== null) : ?>
+       <div class="alert alert-danger" role="alert">
+         <?php if (is_array(session('errors'))) : ?>
+           <?php foreach (session('errors') as $error) : ?>
+             <?= $error ?>
+             <br>
+           <?php endforeach ?>
+         <?php else : ?>
+           <?= session('errors') ?>
+         <?php endif ?>
+       </div>
+     <?php endif ?>
+
      <div class="ui raised segment">
-       <div class="column q-mt-sm">
-         <div class="bold-text" style="margin-top: 10px;">Full Name :</div>
-         <div class="ui labeled input fluid">
-           <input type="text" id="fullname" name="fullname" placeholder="Please Enter Full Name">
-         </div>
+       <form action="<?= url_to('register') ?>" method="post" class="column q-mt-sm">
+         <?= csrf_field() ?>
+
+         <!-- username -->
          <div class="bold-text" style="margin-top: 10px;">Username :</div>
          <div class="column q-mt-sm">
            <div class="ui labeled input fluid">
              <input type="text" id="username" name="username" placeholder="Please Enter Username">
            </div>
          </div>
+
+         <!-- email -->
          <div class="bold-text" style="margin-top: 10px;">Email :</div>
          <div class="column q-mt-sm">
            <div class="ui labeled input fluid">
              <input type="email" id="email" name="email" placeholder="Please Enter Email">
            </div>
          </div>
+
+         <!-- password -->
          <div class="bold-text" style="margin-top: 10px;">Password :</div>
          <div class="column">
            <div class="ui labeled input fluid">
              <input type="password" id="password" name="password" placeholder="Please Enter Password">
            </div>
          </div>
+
+         <!-- confirm password -->
          <div class="bold-text" style="margin-top: 10px;">Re-enter Your Password :</div>
          <div class="column">
            <div class="ui labeled input fluid">
-             <input type="repassword" id="repassword" name="repassword" placeholder="Please Re-enter The Password">
+             <input type="password" id="password_confirm" name="password_confirm" placeholder="Please Re-enter The Password">
            </div>
          </div>
-         <div class="bold-text" style="margin-top: 10px;">
-           <input type="checkbox" tabindex="0" class="fitted">
-           <label>I agree to the Terms and Conditions</label>
-         </div>
+
          <!-- Add more form fields as needed for registration -->
          <div class="column q-mt-sm" style="margin-top: 10px;">
-           <a href="/" class="ui black button flat no-caps" style="margin-top: 10px; margin-left: 10px; margin-right: 10px;">Register</a>
+           <button type="submit" class="ui black button flat no-caps" style="margin-top: 10px; margin-left: 10px; margin-right: 10px;">Register</button>
          </div>
-       </div>
+       </form>
      </div>
    </div>
-
-   <script>
-     // Custom JavaScript functions here, including the submitRegistration() function
-     function submitRegistration() {
-       // Implement the registration form submission
-     }
-   </script>
 
    <?= $this->endSection() ?>
