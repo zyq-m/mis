9aca11f904ab1ab0af7e46b29f43cb65819003f9
Aqief Syahmi <aqiefsyahmi@gmail.com>
2023-08-14 15:10:54 +0800

    adjust img repo page

--- app/Views/image_repo/image_repo.php
+++ app/Views/image_repo/image_repo.php
@@ -9,33 +9,29 @@
             <div class="card">
                 <div class="card-body">
                     <div class="row">
-
-                        <div class="col-md-12">
+                        <div class="col-md-4">
                             <div class="form-group">
                                 <label>Upload Image:</label>
-                                <div>
-                                    <label for="fileInput" class="custom-file-upload" id="customFileLabel">
-                                        <input type="file" name="memoimg" id="fileInput" accept=".jpg, .jpeg, .png" required>
-                                    </label>
-                                    <span id="imageInsertedMessage" style="margin-left: 10px;"></span>
+                                <div class="input-group mb-3">
+                                    <div class="custom-file">
+                                        <input type="file" class="custom-file-input" id="inputGroupFile01">
+                                        <label class="custom-file-label" for="inputGroupFile01">Choose file</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="form-group">
-                                <label>Comment:</label>
-                                <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Comment:</label>
                                     <textarea rows="3" name="descriptions" id="descriptions" class="form-control" placeholder="Please Enter Current Symptoms or Concerns" required></textarea>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="card-footer">
-                    <div>
-                        <button type="reset" class="btn btn-danger btn-reset" id="reset">Reset</button>
-                        <button type="submit" class="btn btn-primary btn-submit" id="submit">Submit Form</button>
+                    <div class="card-footer">
+                        <div>
+                            <button type="reset" class="btn btn-danger btn-reset" id="reset">Reset</button>
+                            <button type="submit" class="btn btn-primary btn-submit" id="submit">Submit Form</button>
+                        </div>
                     </div>
                 </div>
-            </div>
         </form>
     </div>
 </div>
