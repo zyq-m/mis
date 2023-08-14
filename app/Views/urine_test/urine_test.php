6878c5a7fc26470c9b6992697f60f885e6b64498
Haziq Musa <last.game01987@gmail.com>
2023-08-14 23:30:39 +0800

    refactor form

--- app/Views/urine_test/urine_test.php
+++ app/Views/urine_test/urine_test.php
@@ -6,32 +6,37 @@
     <div class="col-md-12">
         <form action="<?= base_url('urine_test') ?>" method="post" class="form-horizontal ui raised segment" id="urineTestForm">
             <?= csrf_field() ?>
+
             <div class="card">
+
                 <div class="card-body">
                     <div class="row">
-
                         <div class="col-md-4">
                             <div class="form-group">
-                                <label>Full Name:</label>
-                                <input type="text" id="fullname" class="form-control" name="fullname" placeholder="Please Enter Full Name" required>
+                                <label for="full_name">Full Name:</label>
+                                <input type="text" id="full_name" class="form-control" name="full_name" placeholder="Enter Full Name" value="<?= set_value('full_name') ?>">
                             </div>
+
                             <div class="form-group">
-                                <label>Date of Birth:</label>
-                                <input type="date" class="form-control" id="dob" name="dob" required>
+                                <label for="date_of_birth">Date of Birth:</label>
+                                <input type="date" class="form-control" id="date_of_birth" name="date_of_birth" value="<?= set_value('date_of_birth') ?>">
                             </div>
+
                             <div class="form-group">
-                                <label>Current Symptoms or Concerns:</label>
-                                <textarea rows="3" class="form-control" placeholder="Please Enter Current Symptoms or Concerns" name="descriptions" required></textarea>
+                                <label for="descriptions">Current Symptoms or Concerns:</label>
+                                <textarea rows="3" class="form-control" placeholder="Enter Current Symptoms or Concerns" name="descriptions"><?= set_value('descriptions') ?></textarea>
                             </div>
                         </div>
                     </div>
                 </div>
+
                 <div class="card-footer">
                     <div>
-                        <button type="reset" class="btn btn-danger btn-reset" id="reset">Reset</button>
-                        <button type="submit" class="btn btn-primary btn-submit" id="submit">Submit Request</button>
+                        <button type="reset" class="btn btn-default" id="reset">Cancel</button>
+                        <button type="submit" class="btn btn-primary" id="submit">Submit</button>
                     </div>
                 </div>
+
             </div>
         </form>
     </div>
