ecc9db8cfa99e48d452a79956e9ce8bc8e0c39ee
Haziq Musa <last.game01987@gmail.com>
2023-08-16 15:10:03 +0800

    view file for img-repo

--- app/Views/image_repo/index.php
+++ app/Views/image_repo/index.php
@@ -4,7 +4,10 @@
 <div>
     <?php if (!empty($images)) : ?>
         <?php foreach ($images as $image) : ?>
-            <img src="<?= esc($image['path']) ?>" class="img-reponsive" alt="Image Repository">
+            <div>
+                <img src="<?= esc($image['path']) ?>" class="img-reponsive" alt="Image Repository">
+                <p>$<?= esc($image['descriptions']) ?></p>
+            </div>
         <?php endforeach; ?>
     <?php endif; ?>
 </div>
