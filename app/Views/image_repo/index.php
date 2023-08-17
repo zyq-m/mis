d42af93de78c8401a86c0332315d3deeb3943129
Haziq Musa <last.game01987@gmail.com>
2023-08-17 11:55:52 +0800

    refactor

--- app/Views/image_repo/index.php
+++ app/Views/image_repo/index.php
@@ -4,9 +4,10 @@
 <div>
     <?php if (!empty($images)) : ?>
         <?php foreach ($images as $image) : ?>
+
             <div>
-                <img src="<?= esc($image['path']) ?>" class="img-reponsive" alt="Image Repository">
-                <p>$<?= esc($image['descriptions']) ?></p>
+                <img src="<?= str_contains($image['path'], 'memo-img') ? base_url('image/' . esc($image['path'])) : esc($image['path']) ?>" class="img-reponsive" alt="Image Repository">
+                <p><?= esc($image['descriptions']) ?></p>
             </div>
         <?php endforeach; ?>
     <?php endif; ?>
