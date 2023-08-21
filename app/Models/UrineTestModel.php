76da2a4c3eec0b6bf9949e356408af480e8813c9
Haziq Musa <last.game01987@gmail.com>
2023-08-22 00:17:10 +0800

    adding new db field

--- app/Models/UrineTestModel.php
+++ app/Models/UrineTestModel.php
@@ -9,5 +9,21 @@ class UrineTestModel extends Model
     protected $table = "urine_test";
     protected $primaryKey = "id";
 
-    protected $allowedFields = ['full_name', 'date_of_birth', 'descriptions'];
+    protected $allowedFields = [
+        'id',
+        'descriptions',
+        'blood',
+        'bilirubin',
+        'urobilinogen',
+        'keton',
+        'protein',
+        'nitrit',
+        'glucose',
+        'pH',
+        's_gravity',
+        'leukocytes',
+        'test_taken',
+        'test_result',
+        'patient_id'
+    ];
 }
