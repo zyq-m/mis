8a271f6db968987b957965d2eee1d0e0438cc7f9
Haziq Musa <last.game01987@gmail.com>
2023-08-22 16:11:53 +0800

    refactor

--- app/Views/patient/view.php
+++ app/Views/patient/view.php
@@ -4,100 +4,51 @@
 
 <!-- utk view specific patient -->
 
-<?php if (!empty($patient) && is_array($patient)) : ?>
 
-    <?php foreach ($patient as $patient_details) : ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="ui raised segment">
+            <div class="card">
+                <div class="card-body">
+                    <ul class="nav nav-pills" id="myTab" role="tablist">
+                        <li role="presentation">
+                            <button class="btn nav-link active" id="profile-tab" data-toggle="tab" data-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="true">Profile</button>
+                        </li>
+                        <li role="presentation">
+                            <button class="btn nav-link" id="urine-tab" data-toggle="tab" data-target="#urine" type="button" role="tab" aria-controls="urine" aria-selected="false">Urine Test</button>
+                        </li>
+                        <li role="presentation">
+                            <button class="btn nav-link" id="repo-tab" data-toggle="tab" data-target="#repo" type="button" role="tab" aria-controls="repo" aria-selected="false">Image Repository</button>
+                        </li>
+                    </ul>
+                    <?php if (!empty($patient) && is_array($patient)) : ?>
 
-        <div class="row">
-            <div class="col-md-12">
-                <div class="ui raised segment">
-                    <div class="card">
-                        <div class="card-body">
-                            <div class="row">
-                                <div class="card-body">
-                                    <div class="card-body text-center">
-                                        <img src="<?= esc($patient_details['avatar']) ?>" alt="avatar" class="rounded-circle img-fluid" style="width: 170px; height: 170px;">
-                                        <br>
-                                        <br>
-                                    </div>
-                                    <div class="row">
-                                        <div class="col-sm-3">
-                                            <p class="mb-0">Full Name</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0"><?= esc($patient_details['name']) ?></p>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3">
-                                            <p class="mb-0">Gender</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0"><?= esc($patient_details['gender']) ?></p>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3">
-                                            <p class="mb-0">IC No.</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0"><?= esc($patient_details['ic_no']) ?></p>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3">
-                                            <p class="mb-0">Date of Birth</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">12/12/2001</p>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3">
-                                            <p class="mb-0">Age</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0">22 years-old</p>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3">
-                                            <p class="mb-0">Phone No.</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0"><?= esc($patient_details['phone_number']) ?></p>
-                                        </div>
-                                    </div>
-                                    <hr>
-                                    <div class="row">
-                                        <div class="col-sm-3">
-                                            <p class="mb-0">Address</p>
-                                        </div>
-                                        <div class="col-sm-9">
-                                            <p class="text-muted mb-0"><?= esc($patient_details['address']) ?></p>
-                                        </div>
-                                    </div>
-                                </div>
+                        <div class="tab-content" id="myTabContent">
+                            <!-- Profile -->
+                            <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                                <?= view('patient/details/profile', ['patient' => $patient]) ?>
+                            </div>
+
+                            <!-- Urine Test -->
+                            <div class="tab-pane fade show active" id="urine" role="tabpanel" aria-labelledby="urine-tab">
+                                <?= view('patient/details/urine_test', ['patient' => $patient]) ?>
+                            </div>
+
+                            <!-- Image Repository -->
+                            <div class="tab-pane fade" id="repo" role="tabpanel" aria-labelledby="repo-tab">
+                                <pre><?= json_encode($patient['img_repo'], JSON_PRETTY_PRINT) ?></pre>
                             </div>
                         </div>
-                    </div>
+
+                    <?php else : ?>
+                        <h3>No patient found</h3>
+
+                        <p>Unable to find patient with id <b><?= esc($id) ?></b></p>
+
+                    <?php endif ?>
+                    <?= $this->endSection(); ?>
                 </div>
             </div>
         </div>
-
-    <?php endforeach ?>
-
-<?php else : ?>
-
-    <h3>No patient found</h3>
-
-    <p>Unable to find patient with id <b><?= esc($id) ?></b></p>
-
-<?php endif ?>
-
-<?= $this->endSection(); ?>
+    </div>
+</div>
