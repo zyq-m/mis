f142fe345f1188597a97c5d860393b602089995c
Haziq Musa <last.game01987@gmail.com>
2023-08-23 16:09:46 +0800

    change ui

--- app/Views/image_repo/index.php
+++ app/Views/image_repo/index.php
@@ -1,16 +1,58 @@
+<?= $this->section("stylesheet"); ?>
+<link rel="stylesheet" href="<?= base_url('assets/v3/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') ?>">
+<link rel="stylesheet" href="<?= base_url('assets/v3/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') ?>">
+<link rel="stylesheet" href="<?= base_url('assets/v3/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') ?>">
+<?= $this->endSection() ?>
+
 <?= $this->extend("template/layout"); ?>
 <?= $this->section("content"); ?>
 
-<div>
-    <?php if (!empty($images)) : ?>
-        <?php foreach ($images as $image) : ?>
-
-            <div>
-                <img src="<?= str_contains($image['path'], 'memo-img') ? base_url('image/' . esc($image['path'])) : esc($image['path']) ?>" class="img-reponsive" alt="Image Repository">
-                <p><?= esc($image['descriptions']) ?></p>
+<div class="card">
+    <div class="card-body">
+        <?php if (!empty($images)) : ?>
+            <div class="table-responsive">
+                <table class="table table-borderless table-hover" id="tblist">
+                    <thead>
+                        <tr>
+                            <th scope="col">Name</th>
+                            <th scope="col">Date taken</th>
+                            <th scope="col">Time</th>
+                            <th scope="col">Date uploaded</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($images as $image) : ?>
+                            <tr>
+                                <td scope="row">
+                                    <img src="<?= base_url('image/' . $image['path']) ?>" alt="..." class="img-fluid mr-1" style="height: 20px; object-fit: cover; width: 20px;">
+                                    <a href="<?= base_url('image/' . $image['path']) ?>" target="_blank">
+                                        <?= $image['path'] ?>
+                                    </a>
+                                </td>
+                                <td><?= $image['screening_date'] ?></td>
+                                <td><?= $image['screening_time'] ?></td>
+                                <td class="text-muted"><?= $image['time_stamp'] ?></td>
+                            </tr>
+                        <?php endforeach ?>
+                    </tbody>
+                </table>
             </div>
-        <?php endforeach; ?>
-    <?php endif; ?>
+        <?php endif; ?>
+    </div>
+
 </div>
 
 <?= $this->endSection(); ?>
+
+<?= $this->section("scripts"); ?>
+<script src="<?= base_url('assets/v3/plugins/datatables/jquery.dataTables.min.js') ?>"></script>
+<script src="<?= base_url('assets/v3/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') ?>"></script>
+<script src="<?= base_url('assets/v3/plugins/datatables-responsive/js/dataTables.responsive.min.js') ?>"></script>
+<script src="<?= base_url('assets/v3/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') ?>"></script>
+<script>
+    $(function() {
+        $('#tblist').DataTable();
+
+    });
+</script>
+<?= $this->endSection() ?>
