bf64afa861cbc38bce98887d6c30889c1477d4a5
Haziq Musa <last.game01987@gmail.com>
2023-08-23 10:46:35 +0800

    update view

--- app/Views/patient/details/urine_test.php
+++ app/Views/patient/details/urine_test.php
@@ -1,6 +1,6 @@
-<div class="row">
+<div class="row mt-4 gap-4">
     <?php foreach ($patient['urine_test'] as $patient_details) : ?>
-        <div class="col-md-4 mt-4 table-responsive-sm">
+        <div class="col-md-4 table-responsive-sm">
             <table class="table table-bordered">
                 <tr>
                     <td scope="row">Blood</td>
@@ -41,32 +41,13 @@
             </table>
 
             <div class="row justify-content-end">
-                <div class="col-md-auto font-weight-bold">Date of Test</div>
-                <div class="col-md-auto"><?= $patient_details['test_taken'] ?></div>
+                <div class="col-sm-auto font-weight-bold">Date of Test</div>
+                <div class="col-sm-auto"><?= $patient_details['test_taken'] ?></div>
             </div>
             <div class="row justify-content-end">
-                <div class="col-md-auto font-weight-bold">Date of Result</div>
-                <div class="col-md-auto"><?= $patient_details['test_result'] ?></div>
+                <div class="col-sm-auto font-weight-bold">Date of Result</div>
+                <div class="col-sm-auto"><?= $patient_details['test_result'] ?></div>
             </div>
         </div>
     <?php endforeach ?>
 </div>
-
-<!-- {
-"id": "6",
-"descriptions": "",
-"blood": "1",
-"bilirubin": "1",
-"urobilinogen": "1",
-"keton": "1",
-"protein": "1",
-"nitrit": "1",
-"glucose": "1",
-"pH": "1",
-"s_gravity": "1",
-"leukocytes": "1",
-"test_taken": "2023-08-22",
-"test_result": "2023-08-22",
-"patient_id": "2",
-"time_stamp": "2023-08-22 00:26:35"
-}, -->
--- app/Views/patient/view.php
+++ app/Views/patient/view.php
@@ -1,27 +1,38 @@
 <?= $this->extend("template/layout"); ?>
 
+<?= $this->section("stylesheet"); ?>
+
+<style>
+    .gap-4 {
+        gap: 1.5rem;
+    }
+</style>
+
+<?= $this->endSection(); ?>
+
 <?= $this->section("content"); ?>
 
 <!-- utk view specific patient -->
-
 
 <div class="row">
     <div class="col-md-12">
         <div class="ui raised segment">
             <div class="card">
-                <div class="card-body">
-                    <ul class="nav nav-pills" id="myTab" role="tablist">
+                <div class="card-header">
+                    <ul class="nav nav-tabs card-header-tabs" id="myTab" role="tablist">
                         <li role="presentation">
-                            <button class="btn nav-link active" id="profile-tab" data-toggle="tab" data-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="true">Profile</button>
+                            <button class="btn nav-link" id="profile-tab" data-toggle="tab" data-target="#profile" type="button" role="tab" aria-controls="profile" aria-selected="true">Profile</button>
                         </li>
                         <li role="presentation">
                             <button class="btn nav-link" id="urine-tab" data-toggle="tab" data-target="#urine" type="button" role="tab" aria-controls="urine" aria-selected="false">Urine Test</button>
                         </li>
                         <li role="presentation">
-                            <button class="btn nav-link" id="repo-tab" data-toggle="tab" data-target="#repo" type="button" role="tab" aria-controls="repo" aria-selected="false">Image Repository</button>
+                            <button class="btn nav-link active" id="repo-tab" data-toggle="tab" data-target="#repo" type="button" role="tab" aria-controls="repo" aria-selected="false">Image Repository</button>
                         </li>
                     </ul>
-                    <?php if (!empty($patient) && is_array($patient)) : ?>
+                </div>
+                <div class="card-body">
+                    <?php if (!empty($patient) && is_array($patient['profile'])) : ?>
 
                         <div class="tab-content" id="myTabContent">
                             <!-- Profile -->
@@ -30,12 +41,12 @@
                             </div>
 
                             <!-- Urine Test -->
-                            <div class="tab-pane fade show active" id="urine" role="tabpanel" aria-labelledby="urine-tab">
+                            <div class="tab-pane fade" id="urine" role="tabpanel" aria-labelledby="urine-tab">
                                 <?= view('patient/details/urine_test', ['patient' => $patient]) ?>
                             </div>
 
                             <!-- Image Repository -->
-                            <div class="tab-pane fade" id="repo" role="tabpanel" aria-labelledby="repo-tab">
+                            <div class="tab-pane fade show active" id="repo" role="tabpanel" aria-labelledby="repo-tab">
                                 <pre><?= json_encode($patient['img_repo'], JSON_PRETTY_PRINT) ?></pre>
                             </div>
                         </div>
@@ -46,9 +57,10 @@
                         <p>Unable to find patient with id <b><?= esc($id) ?></b></p>
 
                     <?php endif ?>
-                    <?= $this->endSection(); ?>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+<?= $this->endSection(); ?>
