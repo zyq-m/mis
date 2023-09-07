d406b5ffdedda0ab45c240b7bb5aedbce4f359e7
Haziq Musa <last.game01987@gmail.com>
2023-09-08 01:40:52 +0800

    popup img feature

---
+++ app/Views/patient/details/img_modal.php
@@ -0,0 +1,15 @@
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Test</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <img src="<?= base_url('image/') ?>" alt="" class="img-fluid">
+            </div>
+        </div>
+    </div>
+</div>
