427a6b30695666e8fc4aec1a00dfa16d3b230fc1
Haziq Musa <last.game01987@gmail.com>
2023-09-08 01:41:08 +0800

    add padding to layout

--- app/Views/template/layout.php
+++ app/Views/template/layout.php
@@ -76,7 +76,7 @@
         <?php echo view('template/usermenu'); ?>
 
         <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
+        <div class="content-wrapper px-4 py-4">
             <!-- Content Header (Page header) -->
             <div class="content-header">
                 <div class="container-fluid">
