28991e5889bee96c270e49babcdc16e95bd97462
Haziq Musa <last.game01987@gmail.com>
2023-09-14 11:39:33 +0800

    change upload image

--- app/Views/patient/register.php
+++ app/Views/patient/register.php
@@ -1,4 +1,17 @@
 <?= $this->extend("template/layout"); ?>
+
+<?= $this->section("stylesheet"); ?>
+<link href="<?= base_url('node_modules/filepond/dist/filepond.css') ?>" rel="stylesheet" />
+<link href="<?= base_url('node_modules/filepond-plugin-image-edit/dist/filepond-plugin-image-edit.css') ?>" rel="stylesheet" />
+<link href="<?= base_url('node_modules/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css') ?>" rel="stylesheet" />
+
+<style>
+    .filepond--root {
+        width: 170px;
+    }
+</style>
+<?= $this->endSection(); ?>
+
 <?= $this->section("content"); ?>
 
 <?php $session = session(); ?>
@@ -22,28 +35,35 @@
     <?= csrf_field() ?>
 
     <div class="card">
-        <div class="card-body px-4">
-            <!-- Indetity -->
-            <?= $this->include('patient/register_details/identity') ?>
-            <!-- End indentity -->
-
-            <!-- Demographic -->
-            <?= $this->include('patient/register_details/demographic') ?>
-            <!-- End Demographic -->
-
-            <!-- Clinical History -->
-            <?= $this->include('patient/register_details/clinical_history') ?>
-            <!-- End Clinical History -->
+        <div class="card-body px-4 row">
+            <div class="ml-2 mr-3">
+                <input type="file" id="img" name="filepond" accept="image/png, image/jpeg, image/gif" />
+            </div>
+            <div class="col">
+                <!-- Indetity -->
+                <?= $this->include('patient/register_details/identity') ?>
+                <!-- End indentity -->
+                <!-- Demographic -->
+                <?= $this->include('patient/register_details/demographic') ?>
+                <!-- End Demographic -->
+                <!-- Clinical History -->
+                <?= $this->include('patient/register_details/clinical_history') ?>
+                <!-- End Clinical History -->
+            </div>
         </div>
-        <div class="card-footer">
-            <div>
-                <button type="reset" class="btn btn-default" id="reset">Cancel</button>
-                <button type="submit" class="btn btn-primary btn-submit" id="submit">Add</button>
+        <div class="card-footer px-4">
+            <div class="row justify-content-end">
+                <div class="col-auto">
+                    <button type="reset" class="btn btn-default" id="reset">Cancel</button>
+                    <button type="submit" class="btn btn-primary btn-submit" id="submit">Add</button>
+                </div>
             </div>
         </div>
     </div>
 
 </form>
+
+<?= $this->endSection(); ?>
 
 <?= $this->section('scripts') ?>
 <script>
@@ -72,10 +92,38 @@
                 document.getElementById(target).style.display = 'none';
             }
         }
-
     }
 </script>
+<script src="<?= base_url('node_modules/filepond/dist/filepond.js') ?>"></script>
+<script src="<?= base_url('node_modules/filepond-plugin-file-validate-size/dist/filepond-plugin-file-validate-size.js') ?>"></script>
+<script src="<?= base_url('node_modules/filepond-plugin-file-validate-type/dist/filepond-plugin-file-validate-type.js') ?>"></script>
+<script src="<?= base_url('node_modules/filepond-plugin-image-crop/dist/filepond-plugin-image-crop.js') ?>"></script>
+<script src="<?= base_url('node_modules/filepond-plugin-image-edit/dist/filepond-plugin-image-edit.js') ?>"></script>
+<script src="<?= base_url('node_modules/filepond-plugin-image-exif-orientation/dist/filepond-plugin-image-exif-orientation.js') ?>"></script>
+<script src="<?= base_url('node_modules/filepond-plugin-image-preview/dist/filepond-plugin-image-preview.js') ?>"></script>
+<script src="<?= base_url('node_modules/filepond-plugin-image-resize/dist/filepond-plugin-image-resize.js') ?>"></script>
+<script src="<?= base_url('node_modules/filepond-plugin-image-transform/dist/filepond-plugin-image-transform.js') ?>"></script>
+<script type="module">
+    FilePond.registerPlugin(
+        FilePondPluginFileValidateType,
+        FilePondPluginImageExifOrientation,
+        FilePondPluginImagePreview,
+        FilePondPluginImageCrop,
+        FilePondPluginImageResize,
+        FilePondPluginImageTransform,
+        FilePondPluginImageEdit
+    );
+    const pond = FilePond.create(document.querySelector('#img'), {
+        labelIdle: `Drag & Drop profile picture or <span class="filepond--label-action">Browse</span>`,
+        imagePreviewHeight: 170,
+        imageCropAspectRatio: '1:1',
+        imageResizeTargetWidth: 200,
+        imageResizeTargetHeight: 200,
+        stylePanelLayout: 'compact circle',
+        styleLoadIndicatorPosition: 'center bottom',
+        styleProgressIndicatorPosition: 'right bottom',
+        styleButtonRemoveItemPosition: 'left bottom',
+        styleButtonProcessItemPosition: 'right bottom',
+    });
+</script>
 <?= $this->endSection() ?>
-
-
-<?= $this->endSection(); ?>
