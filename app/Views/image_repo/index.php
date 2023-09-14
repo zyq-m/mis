3a0158df33e85f9f9749327a127a92f8ab21490f
Haziq Musa <last.game01987@gmail.com>
2023-09-14 22:17:02 +0800

    change layout

--- app/Views/image_repo/index.php
+++ app/Views/image_repo/index.php
@@ -20,19 +20,47 @@
 <?= $this->section("content"); ?>
 
 <div class="card">
+    <div class="card-header">
+        <div class="row justify-content-between align-items-center">
+            <nav aria-label="breadcrumb" class="col-3">
+                <ol class="breadcrumb p-0 m-0 bg-white">
+                    <li class="breadcrumb-item active" aria-current="page"> All </li>
+                </ol>
+            </nav>
+            <div class="row">
+                <form action="" method="get">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                            </span>
+                        </div>
+                        <input type="search" name="input" id="" class="form-control" placeholder="Search for file..">
+                    </div>
+                </form>
+                <div class="col-auto">
+                    <a href="<?= base_url('image_repo/form') ?>" class="btn btn-primary">
+                        <i class="fa fa-plus"></i>
+                        Upload
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="card-body">
         <?php if (!empty($images)) : ?>
-            <div class="row px-2 justify-content-center justify-content-md-start" style="gap: 2rem;">
+            <div class="row justify-content-center justify-content-md-start" style="gap: 2rem;">
                 <?php foreach ($images as $image) : ?>
-                    <div class="col-auto file-card pl-0 pr-0">
-                        <img src="<?= base_url('image/' . $image['path']) ?>" alt="..." class="img-file">
-                        <div class="text-center text-muted text-sm"><?= $image['name'] ?></div>
-                    </div>
+                    <a href="<?= base_url('image_repo/patient/' . $image['myKad']) ?>">
+                        <div class="col-auto file-card pl-0 pr-0 row justify-content-center">
+                            <i class="col-auto fa-solid fa-folder" style="font-size: 14rem;"></i>
+                            <div class="col-auto text-center text-muted text-sm mt-2"><?= $image['name'] ?></div>
+                        </div>
+                    </a>
                 <?php endforeach ?>
             </div>
         <?php endif; ?>
     </div>
-</div>
 </div>
 
 <div class="modal fade" role="dialog" id="imgmodal">
