c700ae4c99f19e43714d7aa7bd38e57ec1534f28
Haziq Musa <last.game01987@gmail.com>
2023-09-19 09:44:11 +0800

    add fall back ui

--- app/Views/patient/details/image_repo.php
+++ app/Views/patient/details/image_repo.php
@@ -7,39 +7,39 @@
 <?php $session = session() ?>
 <?php $is_img = $session->getFlashdata('img') ?>
 
-<div class="table-responsive">
-    <table class="table table-borderless table-hover" id="tblist">
-        <thead>
-            <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Date taken</th>
-                <th scope="col">Time</th>
-                <th scope="col">Date uploaded</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($patient['img_repo'] as $patient_details) : ?>
+<?php if (!empty($patient['img_repo'])) : ?>
+    <div class="table-responsive">
+        <table class="table table-borderless table-hover" id="tblist">
+            <thead>
                 <tr>
-                    <td scope="row">
-                        <img src="<?= base_url('image/' . $patient_details['path']) ?>" alt="..." class="img-fluid mr-1" style="height: 20px; object-fit: cover; width: 20px;">
-                        <a href="<?= base_url('image/' . $patient_details['path']) ?>" target="_blank">
-                            <?= $patient_details['path'] ?>
-                        </a>
-                    </td>
-                    <td><?= $patient_details['screening_date'] ?></td>
-                    <td><?= $patient_details['screening_time'] ?></td>
-                    <td class="text-muted"><?= $patient_details['time_stamp'] ?></td>
+                    <th scope="col">Name</th>
+                    <th scope="col">Date taken</th>
+                    <th scope="col">Time</th>
+                    <th scope="col">Date uploaded</th>
                 </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
-</div>
-
-<?php if ($is_img) : ?>
-    <div>
-        <img src="<?= base_url('image/' . $is_img) ?>" alt="..." class="img-fluid">
+            </thead>
+            <tbody>
+                <?php foreach ($patient['img_repo'] as $patient_details) : ?>
+                    <tr>
+                        <td scope="row">
+                            <img src="<?= base_url('image/' . $patient_details['path']) ?>" alt="..." class="img-fluid mr-1" style="height: 20px; object-fit: cover; width: 20px;">
+                            <a href="<?= base_url('image/' . $patient_details['path']) ?>" target="_blank">
+                                <?= $patient_details['path'] ?>
+                            </a>
+                        </td>
+                        <td><?= $patient_details['screening_date'] ?></td>
+                        <td><?= $patient_details['screening_time'] ?></td>
+                        <td class="text-muted"><?= $patient_details['time_stamp'] ?></td>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
     </div>
-<?php endif; ?>
+<?php else : ?>
+    <div>
+        <div>No record found. Click <a href="<?= base_url('image_repo/form') ?>">here</a> to add.</div>
+    </div>
+<?php endif ?>
 
 <?= $this->section("scripts"); ?>
 <script src="<?= base_url('assets/v3/plugins/datatables/jquery.dataTables.min.js') ?>"></script>
--- app/Views/patient/details/urine_test.php
+++ app/Views/patient/details/urine_test.php
@@ -1,53 +1,59 @@
-<div class="row mt-4 gap-4">
-    <?php foreach ($patient['urine_test'] as $patient_details) : ?>
-        <div class="col-md-4 table-responsive-sm">
-            <table class="table table-bordered">
-                <tr>
-                    <td scope="row">Blood</td>
-                    <td class="text-center"><?= $patient_details['blood'] ?></td>
-                </tr>
-                <tr>
-                    <td scope="row">Bilirubin</td>
-                    <td class="text-center"><?= $patient_details['bilirubin'] ?></td>
-                </tr>
-                <tr>
-                    <td scope="row">Keton</td>
-                    <td class="text-center"><?= $patient_details['keton'] ?></td>
-                </tr>
-                <tr>
-                    <td scope="row">Protein</td>
-                    <td class="text-center"><?= $patient_details['protein'] ?></td>
-                </tr>
-                <tr>
-                    <td scope="row">Nitrit</td>
-                    <td class="text-center"><?= $patient_details['nitrit'] ?></td>
-                </tr>
-                <tr>
-                    <td scope="row">Glucose</td>
-                    <td class="text-center"><?= $patient_details['glucose'] ?></td>
-                </tr>
-                <tr>
-                    <td scope="row">pH</td>
-                    <td class="text-center"><?= $patient_details['pH'] ?></td>
-                </tr>
-                <tr>
-                    <td scope="row">Specific Gravity</td>
-                    <td class="text-center"><?= $patient_details['s_gravity'] ?></td>
-                </tr>
-                <tr>
-                    <td scope="row">Leukocytes</td>
-                    <td class="text-center"><?= $patient_details['leukocytes'] ?></td>
-                </tr>
-            </table>
+<?php if (!empty($patient['urine_test'])) : ?>
+    <div class="row mt-4 gap-4">
+        <?php foreach ($patient['urine_test'] as $patient_details) : ?>
+            <div class="col-md-4 table-responsive-sm">
+                <table class="table table-bordered">
+                    <tr>
+                        <td scope="row">Blood</td>
+                        <td class="text-center"><?= $patient_details['blood'] ?></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Bilirubin</td>
+                        <td class="text-center"><?= $patient_details['bilirubin'] ?></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Keton</td>
+                        <td class="text-center"><?= $patient_details['keton'] ?></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Protein</td>
+                        <td class="text-center"><?= $patient_details['protein'] ?></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Nitrit</td>
+                        <td class="text-center"><?= $patient_details['nitrit'] ?></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Glucose</td>
+                        <td class="text-center"><?= $patient_details['glucose'] ?></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">pH</td>
+                        <td class="text-center"><?= $patient_details['pH'] ?></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Specific Gravity</td>
+                        <td class="text-center"><?= $patient_details['s_gravity'] ?></td>
+                    </tr>
+                    <tr>
+                        <td scope="row">Leukocytes</td>
+                        <td class="text-center"><?= $patient_details['leukocytes'] ?></td>
+                    </tr>
+                </table>
 
-            <div class="row justify-content-end">
-                <div class="col-sm-auto font-weight-bold">Date of Test</div>
-                <div class="col-sm-auto"><?= $patient_details['test_taken'] ?></div>
+                <div class="row justify-content-end">
+                    <div class="col-sm-auto font-weight-bold">Date of Test</div>
+                    <div class="col-sm-auto"><?= $patient_details['test_taken'] ?></div>
+                </div>
+                <div class="row justify-content-end">
+                    <div class="col-sm-auto font-weight-bold">Date of Result</div>
+                    <div class="col-sm-auto"><?= $patient_details['test_result'] ?></div>
+                </div>
             </div>
-            <div class="row justify-content-end">
-                <div class="col-sm-auto font-weight-bold">Date of Result</div>
-                <div class="col-sm-auto"><?= $patient_details['test_result'] ?></div>
-            </div>
-        </div>
-    <?php endforeach ?>
-</div>
+        <?php endforeach ?>
+    </div>
+<?php else : ?>
+    <div>
+        <div>No record found. Click <a href="<?= base_url('urine_test') ?>">here</a> to add.</div>
+    </div>
+<?php endif ?>
