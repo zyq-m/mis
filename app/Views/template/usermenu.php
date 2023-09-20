b073af0f8e4eb841851fd2871fb3546f378f6ef4
Haziq Musa <last.game01987@gmail.com>
2023-09-20 15:43:15 +0800

    refactor active class function

--- app/Views/template/usermenu.php
+++ app/Views/template/usermenu.php
@@ -1,5 +1,4 @@
 <?php
-
 $uri = service('uri');
 
 $menuItems = [
@@ -7,90 +6,43 @@ $menuItems = [
         'id' => 1,
         'name' => 'Dashboard',
         'icon' => 'fa-home',
-        'link' => base_url('dashboard')
+        'link' => site_url('dashboard')
     ],
     [
         'id' => 2,
         'name' => 'Patients',
         'icon' => 'fa-bed-pulse', // You can choose the appropriate icon class
+        'link' => '#',
         'subItems' => [
-            ['id' => 21, 'name' => 'Patients List', 'link' => base_url('patient')],
-            ['id' => 22, 'name' => 'Register Patient', 'link' => base_url('patient/register')],
+            ['id' => 21, 'name' => 'Patients List', 'link' => site_url('patient')],
+            ['id' => 22, 'name' => 'Register Patient', 'link' => site_url('patient/register')],
         ]
     ],
     [
         'id' => 3,
         'name' => 'Image Repository',
         'icon' => 'fa-images', // You can choose the appropriate icon class
+        'link' => '#',
         'subItems' => [
-            ['id' => 52, 'name' => 'Images', 'link' => base_url('image_repo')],
-            ['id' => 53, 'name' => 'Upload', 'link' => base_url('image_repo/form')],
+            ['id' => 52, 'name' => 'Images', 'link' => site_url('image_repo')],
+            ['id' => 53, 'name' => 'Upload', 'link' => site_url('image_repo/form')],
         ]
     ],
     [
         'id' => 5,
         'name' => 'Urine Test',
         'icon' => 'fa-flask', // You can choose the appropriate icon class
+        'link' => '#',
         'subItems' => [
-            ['id' => 51, 'name' => 'Add new test', 'link' => base_url('urine_test')],
+            ['id' => 51, 'name' => 'Add new test', 'link' => site_url('urine_test')],
         ]
     ],
 ];
-
-function getMenuHref($itemId)
-{
-    // Replace this logic with your actual URLs for each menu item
-    switch ($itemId) {
-        case 1:
-            return 'dashboard';
-        case 21:
-            return 'patient';
-        case 22:
-            return 'patient/register';
-        case 51:
-            return 'urine_test';
-        case 52:
-            return 'image_repo';
-        case 53:
-            return 'image_repo/form';
-        default:
-            return '#'; // Default case if no match found
-    }
-}
-
-function isMenuItemActive($itemId)
-{
-    $uri = service('uri');
-    $currentSegment = $uri->getSegment(1); // Assuming the segment for your menu item is at index 1
-    $menuItemSegment = getMenuHref($itemId);
-
-    // Check if the current URL segment matches the menu item segment
-    if ($currentSegment === $menuItemSegment) {
-        return ' active';
-    }
-
-    return '';
-}
-
-function isSubMenuItemActive($subItemId)
-{
-    $uri = service('uri');
-    $currentUrl = current_url(); // Get the current full URL
-    $subMenuItemUrl = base_url(getMenuHref($subItemId)); // Generate sub-menu URL
-
-    // Check if the current URL matches the sub-menu URL
-    if ($currentUrl === $subMenuItemUrl) {
-        return ' active';
-    }
-
-    return '';
-}
-
 ?>
 <aside class="main-sidebar elevation-4 sidebar-dark-pink">
     <!-- Brand Logo -->
     <a href="<?= base_url() ?>dashboard" class="brand-link bg-white">
-        <img src="<?php echo base_url(); ?>assets/images/pink-ribbon.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="<?= base_url('assets/images/pink-ribbon.png'); ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light"><b>BreastCancer</b>IS</span>
     </a>
 
@@ -99,12 +51,12 @@ function isSubMenuItemActive($subItemId)
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-                <img src="<?php echo base_url(); ?>assets/images/avatar.png" class="img-circle elevation-2" alt="User Image">
+                <img src="<?= base_url('assets/images/avatar.png') ?>" class="img-circle elevation-2" alt="User Image">
             </div>
             <div class="info">
-                <p class="d-block text-white">
+                <span class="d-block text-white">
                     <?= auth()->user()->getEmail() ?>
-                </p>
+                </span>
             </div>
         </div>
         <!-- Sidebar Menu -->
@@ -112,7 +64,7 @@ function isSubMenuItemActive($subItemId)
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <?php foreach ($menuItems as $menuItem) : ?>
                     <li class="nav-item">
-                        <a href="<?= (getMenuHref($menuItem['id'])) ?>" class="nav-link<?= isMenuItemActive($menuItem['id']) ?>">
+                        <a href="<?= $menuItem['link'] ?>" class="nav-link <?= current_url() == $menuItem['link'] ? 'active' : '' ?>">
                             <i class="nav-icon fas <?= $menuItem['icon'] ?>"></i>
                             <p class="text">
                                 <?= $menuItem['name'] ?>
@@ -125,7 +77,7 @@ function isSubMenuItemActive($subItemId)
                             <ul class="nav nav-treeview">
                                 <?php foreach ($menuItem['subItems'] as $subItem) : ?>
                                     <li class="nav-item">
-                                        <a href="<?= (getMenuHref($subItem['id'])) ?>" class="nav-link<?= isSubMenuItemActive($subItem['id']) ?>">
+                                        <a href="<?= $subItem['link'] ?>" class="nav-link <?= current_url() == $subItem['link'] ? 'active' : '' ?>">
                                             <div class="d-flex align-items-center">
                                                 <!-- <i class="far fa-circle nav-icon"></i> -->
                                                 <p class="text" style="margin-left: 2rem;"><?= $subItem['name'] ?></p>
@@ -147,7 +99,7 @@ function isSubMenuItemActive($subItemId)
                 </li>
 
                 <li class="nav-item ">
-                    <a href="<?= base_url("profile/password") ?>" class="nav-link<?= ($uri->getSegment(1) == "profile" && $uri->getSegment(2) == "password") ? " active" : "" ?>">
+                    <a href="<?= base_url("profile/password") ?>" class="nav-link <?= ($uri->getSegment(1) == "profile" && $uri->getSegment(2) == "password") ? " active" : "" ?>">
                         <i class="nav-icon fa fa-key"></i>
                         <p class="text">Change Password</p>
                     </a>
