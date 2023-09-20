d35b8061ef010908d5d2b273153ae0bf6d1a2102
Haziq Musa <last.game01987@gmail.com>
2023-09-20 11:18:12 +0800

    change to site_url

--- app/Views/template/usermenu.php
+++ app/Views/template/usermenu.php
@@ -76,7 +76,7 @@ function isSubMenuItemActive($subItemId)
 {
     $uri = service('uri');
     $currentUrl = current_url(); // Get the current full URL
-    $subMenuItemUrl = base_url(getMenuHref($subItemId)); // Generate sub-menu URL
+    $subMenuItemUrl = site_url(getMenuHref($subItemId)); // Generate sub-menu URL
 
     // Check if the current URL matches the sub-menu URL
     if ($currentUrl === $subMenuItemUrl) {
@@ -89,7 +89,7 @@ function isSubMenuItemActive($subItemId)
 ?>
 <aside class="main-sidebar elevation-4 sidebar-dark-pink">
     <!-- Brand Logo -->
-    <a href="<?= base_url() ?>dashboard" class="brand-link bg-white">
+    <a href="<?= site_url() ?>dashboard" class="brand-link bg-white">
         <img src="<?php echo base_url(); ?>assets/images/pink-ribbon.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light"><b>BreastCancer</b>IS</span>
     </a>
@@ -112,7 +112,7 @@ function isSubMenuItemActive($subItemId)
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <?php foreach ($menuItems as $menuItem) : ?>
                     <li class="nav-item">
-                        <a href="<?= base_url(getMenuHref($menuItem['id'])) ?>" class="nav-link<?= isMenuItemActive($menuItem['id']) ?>">
+                        <a href="<?= site_url(getMenuHref($menuItem['id'])) ?>" class="nav-link<?= isMenuItemActive($menuItem['id']) ?>">
                             <i class="nav-icon fas <?= $menuItem['icon'] ?>"></i>
                             <p class="text">
                                 <?= $menuItem['name'] ?>
@@ -125,7 +125,7 @@ function isSubMenuItemActive($subItemId)
                             <ul class="nav nav-treeview">
                                 <?php foreach ($menuItem['subItems'] as $subItem) : ?>
                                     <li class="nav-item">
-                                        <a href="<?= base_url(getMenuHref($subItem['id'])) ?>" class="nav-link<?= isSubMenuItemActive($subItem['id']) ?>">
+                                        <a href="<?= site_url(getMenuHref($subItem['id'])) ?>" class="nav-link<?= isSubMenuItemActive($subItem['id']) ?>">
                                             <div class="d-flex align-items-center">
                                                 <!-- <i class="far fa-circle nav-icon"></i> -->
                                                 <p class="text" style="margin-left: 2rem;"><?= $subItem['name'] ?></p>
@@ -140,21 +140,21 @@ function isSubMenuItemActive($subItemId)
 
                 <li class="nav-header text-bold text-lg">Account</li>
                 <li class="nav-item ">
-                    <a href="<?= base_url("profile") ?>" class=" nav-link<?= ($uri->getSegment(1) == "profile" && $uri->getSegment(2) == "") ? " active" : "" ?>">
+                    <a href="<?= site_url("profile") ?>" class=" nav-link<?= ($uri->getSegment(1) == "profile" && $uri->getSegment(2) == "") ? " active" : "" ?>">
                         <i class="nav-icon fa fa-user"></i>
                         <p class="text">Profile</p>
                     </a>
                 </li>
 
                 <li class="nav-item ">
-                    <a href="<?= base_url("profile/password") ?>" class="nav-link<?= ($uri->getSegment(1) == "profile" && $uri->getSegment(2) == "password") ? " active" : "" ?>">
+                    <a href="<?= site_url("profile/password") ?>" class="nav-link<?= ($uri->getSegment(1) == "profile" && $uri->getSegment(2) == "password") ? " active" : "" ?>">
                         <i class="nav-icon fa fa-key"></i>
                         <p class="text">Change Password</p>
                     </a>
                 </li>
 
                 <li class="nav-item">
-                    <a href="<?= base_url('logout'); ?>" class="nav-link">
+                    <a href="<?= site_url('logout'); ?>" class="nav-link">
                         <!-- <i class="nav-icon far fa-circle text-danger"></i> -->
                         <i class="nav-icon fa fa-right-from-bracket"></i>
                         <p class="text">Log Out</p>
