e7fdf49c478b47715cc4509f53999f07121b262a
Haziq Musa <87070003+zyq-m@users.noreply.github.com>
2023-11-06 00:18:01 +0800

    Add files via upload

---
+++ public/assets/backend/dist/js/pages/dashboard.js
@@ -0,0 +1,53 @@
+/*
+ * Author: Abdullah A Almsaeed
+ * Date: 4 Jan 2014
+ * Description:
+ *      This is a demo file used only for the main dashboard (index.html)
+ **/
+
+$(function () {
+
+  "use strict";
+
+
+  //jvectormap data
+  var visitorsData = {
+    "US": 398, //USA
+    "SA": 400, //Saudi Arabia
+    "CA": 1000, //Canada
+    "DE": 500, //Germany
+    "FR": 760, //France
+    "CN": 300, //China
+    "AU": 700, //Australia
+    "BR": 600, //Brazil
+    "IN": 800, //India
+    "GB": 320, //Great Britain
+    "RU": 3000 //Russia
+  };
+  //World map by jvectormap
+  $('#world-map').vectorMap({
+    map: 'world_mill_en',
+    backgroundColor: "transparent",
+    regionStyle: {
+      initial: {
+        fill: '#e4e4e4',
+        "fill-opacity": 1,
+        stroke: 'none',
+        "stroke-width": 0,
+        "stroke-opacity": 1
+      }
+    },
+    series: {
+      regions: [{
+        values: visitorsData,
+        scale: ["#92c1dc", "#ebf4f9"],
+        normalizeFunction: 'polynomial'
+      }]
+    },
+    onRegionLabelShow: function (e, el, code) {
+      if (typeof visitorsData[code] != "undefined")
+        el.html(el.html() + ': ' + visitorsData[code] + ' new visitors');
+    }
+  });
+
+});
