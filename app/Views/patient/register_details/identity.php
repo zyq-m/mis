42b8dcd7e7a6de8913d021b9ca755a69da227b60
zyq-m <last.game01987@gmail.com>
2023-12-27 11:23:49 +0800

    update form helper

--- app/Views/patient/register_details/identity.php
+++ app/Views/patient/register_details/identity.php
@@ -4,12 +4,14 @@ $nameInput = [
     'id' => 'name',
     'placeholder' => 'Alisa binti Ahmad',
     'class' => validation_show_error('name') ? 'form-control is-invalid' : 'form-control',
+    'value' => set_value('name'),
 ];
 $myKadInput = [
     'name' => 'myKad',
     'id' => 'myKad',
     'placeholder' => '1234567890',
     'class' => validation_show_error('myKad') ? 'form-control is-invalid' : 'form-control',
+    'value' => set_value('myKad'),
 ];
 $emailInput = [
     'name' => 'email',
@@ -17,12 +19,14 @@ $emailInput = [
     'placeholder' => 'example@email.com',
     'class' => validation_show_error('email') ? 'form-control is-invalid' : 'form-control',
     'type' => 'email',
+    'value' => set_value('email'),
 ];
 $phoneInput = [
     'name' => 'phone_number',
     'id' => 'phone_number',
     'placeholder' => '01234567890',
     'class' => validation_show_error('phone_number') ? 'form-control is-invalid' : 'form-control',
+    'value' => set_value('phone_number'),
 ];
 ?>
 
